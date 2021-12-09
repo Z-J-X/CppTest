4528d013581a89cea9d315108b9e354c901a6cb8
jianxinzhou <jianxinzhou@kaifa.cn>
2021-12-09 08:41:54 +0800

    update

--- Boost/main.cpp
+++ Boost/main.cpp
@@ -1,22 +1,21 @@
-#include <boost/asio.hpp> 
-#include <boost/asio/ip/tcp.hpp>
-#include <iostream>
-
+#include<iostream>
+#include<boost/asio.hpp>
+#include<boost/asio/ip/tcp.hpp>
 int main()
 {
     try
     {
+        auto host = boost::asio::ip::host_name();//获取主机名
+        std::cout << host << std::endl;
         boost::asio::io_service m_io;
         boost::asio::ip::tcp::resolver m_re(m_io);
-        boost::asio::ip::tcp::resolver::query m_q("::", "80");//网址和端口号
+        boost::asio::ip::tcp::resolver::query m_q(host, "");//网址和端口号
         boost::asio::ip::tcp::resolver::iterator it = m_re.resolve(m_q);//同步解析
         decltype(it) end;
         boost::system::error_code ec = boost::asio::error::host_not_found;
         for (; ec && it != end; it++)
         {
-            std::cout << (it->endpoint().protocol() == boost::asio::ip::tcp::v6()) << std::endl;
-            //sock.close();
-            //sock.connect(*it,ec);
+            std::cout << it->endpoint().address() << std::endl;
         }
     }
     catch (std::exception& e)
--- CppTest/main.cpp
+++ CppTest/main.cpp
@@ -1,24 +1,36 @@
-#if 1
 #include<iostream>
-#include<tuple>
+#include<string>
 #include<map>
 #include<chrono>
-//int func(int a)
-//{
-//	return a;
-//}
-//int main()
-//{
-//	int a = 100;
-//	int& b = func(a);
-//}
 
+struct ci_less //: std::binary_function<std::string, std::string, bool>
+{
+	// case-independent (ci) compare_less binary function
+	struct nocase_compare //: public std::binary_function<unsigned char, unsigned char, bool>
+	{
+		bool operator()(const unsigned char& c1, const unsigned char& c2) const
+		{
+			return tolower(c1) < tolower(c2);
+		}
+	};
+	bool operator()(const std::string& s1, const std::string& s2) const
+	{
+		return std::lexicographical_compare(s1.begin(), s1.end(), // source range
+			s2.begin(), s2.end(), // dest range
+			nocase_compare());    // comparison
+	}
+};
 
 int main()
 {
-	std::chrono::system_clock::time_point timePoint = std::chrono::system_clock::now();//stl库获取当前时间
-	 int a = std::chrono::system_clock::to_time_t(timePoint);
-	 std::cout << a << std::endl;
+	std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds> timePoint =
+		std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now());
+	auto timeNow = std::chrono::duration_cast<std::chrono::milliseconds>(timePoint.time_since_epoch());
+	size_t nTime = timeNow.count();//获取时间戳精确到微秒
+
+	double a = 1638845727.6457758;
+	size_t t = a * 1000-3000;
+	std::cout << t << std::endl;
+	std::cout << nTime << std::endl;
 	return 0;
 }
-#endif
